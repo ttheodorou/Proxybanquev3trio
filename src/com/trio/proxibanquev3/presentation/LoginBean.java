d32bc43625294d5b0f312cdeaaace343b049b617
Guillaume Rio <guigzrio@gmail.com>
2016-09-13 02:46:57 +0200

    Change layout and add conseiller in loginBean

--- src/com/trio/proxibanquev3/presentation/LoginBean.java
+++ src/com/trio/proxibanquev3/presentation/LoginBean.java
@@ -1,5 +1,6 @@
 package com.trio.proxibanquev3.presentation;
 
+import com.trio.proxibanquev3.domaine.Conseiller;
 import com.trio.proxibanquev3.exception.DAOException;
 import com.trio.proxibanquev3.service.AuthService;
 import com.trio.proxibanquev3.service.ConseillerService;
@@ -21,6 +22,7 @@ public class LoginBean implements Serializable{
     private static final long serialVersionUID = 1L;
 
 
+
     private String login;
     private String password;
 
@@ -29,6 +31,7 @@ public class LoginBean implements Serializable{
 //    @Inject
     private AuthService authService;
     private ConseillerService conseillerService;
+    private Conseiller conseiller;
 
 
     @ManagedProperty(value="#{navigateBean}")
@@ -39,6 +42,7 @@ public class LoginBean implements Serializable{
         loggedIn = false;
         authService = new AuthService();
         conseillerService = new ConseillerService();
+        conseiller = null;
     }
 
     public String doLogin(){
@@ -48,10 +52,12 @@ public class LoginBean implements Serializable{
 
              if(isValidConseiller){
                  loggedIn = true;
+                 conseiller = conseillerService.lireUnConseiller(login);
                  return navigateBean.redirectToMenuConseiller();
 
              }else{
                  loggedIn = false;
+                 conseiller = null;
 
                  FacesMessage msg = new FacesMessage("Erreur de login ou de password !", "MESSAGE D'ERREUR");
                  msg.setSeverity(FacesMessage.SEVERITY_ERROR);
@@ -64,6 +70,7 @@ public class LoginBean implements Serializable{
 
             e.printStackTrace();
             loggedIn = false;
+            conseiller = null;
 
             return navigateBean.redirectToError(e.getMessage());
 
@@ -75,6 +82,9 @@ public class LoginBean implements Serializable{
         loggedIn = false;
         password = null;
         login = null;
+
+        conseiller = null;
+
         return navigateBean.redirectToLogin();
     }
 
@@ -110,5 +120,13 @@ public class LoginBean implements Serializable{
 
     public NavigateBean getNavigateBean(){
         return navigateBean;
+    }
+
+    public Conseiller getConseiller() {
+        return conseiller;
+    }
+
+    public void setConseiller(Conseiller conseiller) {
+        this.conseiller = conseiller;
     }
 }
