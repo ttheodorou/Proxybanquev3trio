24015710fd1fdb0859234eea3a8a16b0faf0fecf
thomas theodorou <thomas.theodorou@live.fr>
2016-09-13 10:50:10 +0200

    page de virements

--- src/com/trio/proxibanquev3/presentation/VirementBean.java
+++ src/com/trio/proxibanquev3/presentation/VirementBean.java
@@ -18,7 +18,9 @@ import org.primefaces.event.UnselectEvent;
 import com.trio.proxibanquev3.domaine.Client;
 import com.trio.proxibanquev3.domaine.CompteBancaire;
 import com.trio.proxibanquev3.exception.DAOException;
+import com.trio.proxibanquev3.exception.ServiceException;
 import com.trio.proxibanquev3.service.ClientService;
+import com.trio.proxibanquev3.service.CompteBancaireService;
 
 @ManagedBean(name = "virementbean")
 @SessionScoped
@@ -31,27 +33,169 @@ public class VirementBean implements Serializable {
 
 	private List<Client> listeDesClients = new ArrayList<Client>();
 	private List<CompteBancaire> listeDesComptes = new ArrayList<CompteBancaire>();
-	private String client;
-	private String compte;
-	private Map<String, String> clients;
-	private Map<String, String> comptes;
+	private String client1;
+	private String compte1;
+	private Map<String, String> clients1;
+	private Map<String, String> comptes1;
+
+	private String client2;
+	private String compte2;
+	private Map<String, String> clients2;
+	private Map<String, String> comptes2;
 
 	private Map<String, Map<String, String>> data = new HashMap<String, Map<String, String>>();
 
+	private String sommeADebiter;
+
+	/**
+	 * @return the sommeADebiter
+	 */
+	public String getSommeADebiter() {
+		return sommeADebiter;
+	}
+
+	/**
+	 * @param sommeADebiter
+	 *            the sommeADebiter to set
+	 */
+	public void setSommeADebiter(String sommeADebiter) {
+		this.sommeADebiter = sommeADebiter;
+	}
+
+	/**
+	 * @return the client1
+	 */
+	public String getClient1() {
+		return client1;
+	}
+
+	/**
+	 * @param client1
+	 *            the client1 to set
+	 */
+	public void setClient1(String client1) {
+		this.client1 = client1;
+	}
+
+	/**
+	 * @return the compte1
+	 */
+	public String getCompte1() {
+		return compte1;
+	}
+
+	/**
+	 * @param compte1
+	 *            the compte1 to set
+	 */
+	public void setCompte1(String compte1) {
+		this.compte1 = compte1;
+	}
+
+	/**
+	 * @return the clients1
+	 */
+	public Map<String, String> getClients1() {
+		return clients1;
+	}
+
+	/**
+	 * @param clients1
+	 *            the clients1 to set
+	 */
+	public void setClients1(Map<String, String> clients1) {
+		this.clients1 = clients1;
+	}
+
+	/**
+	 * @return the comptes1
+	 */
+	public Map<String, String> getComptes1() {
+		return comptes1;
+	}
+
+	/**
+	 * @param comptes1
+	 *            the comptes1 to set
+	 */
+	public void setComptes1(Map<String, String> comptes1) {
+		this.comptes1 = comptes1;
+	}
+
+	/**
+	 * @return the client2
+	 */
+	public String getClient2() {
+		return client2;
+	}
+
+	/**
+	 * @param client2
+	 *            the client2 to set
+	 */
+	public void setClient2(String client2) {
+		this.client2 = client2;
+	}
+
+	/**
+	 * @return the compte2
+	 */
+	public String getCompte2() {
+		return compte2;
+	}
+
+	/**
+	 * @param compte2
+	 *            the compte2 to set
+	 */
+	public void setCompte2(String compte2) {
+		this.compte2 = compte2;
+	}
+
+	/**
+	 * @return the clients2
+	 */
+	public Map<String, String> getClients2() {
+		return clients2;
+	}
+
+	/**
+	 * @param clients2
+	 *            the clients2 to set
+	 */
+	public void setClients2(Map<String, String> clients2) {
+		this.clients2 = clients2;
+	}
+
+	/**
+	 * @return the comptes2
+	 */
+	public Map<String, String> getComptes2() {
+		return comptes2;
+	}
+
+	/**
+	 * @param comptes2
+	 *            the comptes2 to set
+	 */
+	public void setComptes2(Map<String, String> comptes2) {
+		this.comptes2 = comptes2;
+	}
+
 	public Map<String, String> getClients() {
-		return clients;
+		return clients1;
 	}
 
 	public void setClients(Map<String, String> clients) {
-		this.clients = clients;
+		this.clients1 = clients;
 	}
 
 	public Map<String, String> getComptes() {
-		return comptes;
+		return comptes1;
 	}
 
 	public void setComptes(Map<String, String> comptes) {
-		this.comptes = comptes;
+		this.comptes1 = comptes;
 	}
 
 	public void setData(Map<String, Map<String, String>> data) {
@@ -64,7 +208,7 @@ public class VirementBean implements Serializable {
 	public void init() {
 		try {
 			listeDesClients = clientService.lireToutesLesClients();
-			clients = new HashMap<String, String>();
+			clients1 = new HashMap<String, String>();
 			int tailleListeDesClients = listeDesClients.size();
 
 			for (int i = 0; i < tailleListeDesClients; i++) {
@@ -72,7 +216,7 @@ public class VirementBean implements Serializable {
 				String nom = listeDesClients.get(i).getNom();
 				long id = listeDesClients.get(i).getIdPersonne();
 				String prenomEtNom = prenom + " " + nom + ", id : " + String.valueOf(id);
-				clients.put(prenomEtNom, prenomEtNom);
+				clients1.put(prenomEtNom, prenomEtNom);
 				Map<String, String> map = new HashMap<String, String>();
 				listeDesComptes = listeDesClients.get(i).getComptes();
 				int tailleListeDesComptes = listeDesComptes.size();
@@ -83,13 +227,12 @@ public class VirementBean implements Serializable {
 					String solde = String.valueOf(sld);
 					long idCompte = listeDesComptes.get(j).getIdCompte();
 					String idCompteStr = String.valueOf(idCompte);
-					String numEtSolde = numeroDeCompte + " solde de : " + solde + " � , id =" + idCompteStr;
+					String numEtSolde = numeroDeCompte + " solde de : " + solde + " € , id =" + idCompteStr;
 					map.put(numEtSolde, numEtSolde);
 				}
 				data.put(prenomEtNom, map);
 
 			}
-			System.out.println("");
 
 		} catch (DAOException e) {
 			// TODO Auto-generated catch block
@@ -129,33 +272,33 @@ public class VirementBean implements Serializable {
 	}
 
 	/**
-	 * @return the client
+	 * @return the client1
 	 */
 	public String getClient() {
-		return client;
+		return client1;
 	}
 
 	/**
-	 * @param client
-	 *            the client to set
+	 * @param client1
+	 *            the client1 to set
 	 */
 	public void setClient(String client) {
-		this.client = client;
+		this.client1 = client;
 	}
 
 	/**
-	 * @return the compte
+	 * @return the compte1
 	 */
 	public String getCompte() {
-		return compte;
+		return compte1;
 	}
 
 	/**
-	 * @param compte
-	 *            the compte to set
+	 * @param compte1
+	 *            the compte1 to set
 	 */
 	public void setCompte(String compte) {
-		this.compte = compte;
+		this.compte1 = compte;
 	}
 
 	public Map<String, Map<String, String>> getData() {
@@ -164,25 +307,56 @@ public class VirementBean implements Serializable {
 
 	public void onCountryChange() {
 
-		if (client != null && !client.equals(""))
-			comptes = data.get(client);
+		if (client1 != null && !client1.equals(""))
+			comptes1 = data.get(client1);
 		else
-			comptes = new HashMap<String, String>();
+			comptes1 = new HashMap<String, String>();
 	}
 
-	public void displayLocation() {
-		FacesMessage msg;
-		if (compte != null && client != null)
-//			String compte = "voici un chiffre =554";
-//		String resultat = compte.substring(compte.lastIndexOf("=")+1, compte.length());
-//		long idCompte = Long.parseLong(resultat);
-//		System.out.println(idCompte);
-		
-			msg = new FacesMessage("Selected", compte + " of " + client);
-		else
-			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid", "City is not selected.");
+	public void onCountryChange2() {
 
+		if (client2 != null && !client2.equals(""))
+			comptes2 = data.get(client2);
+		else
+			comptes2 = new HashMap<String, String>();
+	}
+
+	public String displayLocation() {
+		FacesMessage msg = null;
+		String resultat;
+		Long idCompte1;
+		String resultat2;
+		Long idCompte2;
+		double montant;
+		CompteBancaire compteDebite = null;
+		CompteBancaire compteCredite = null;
+		CompteBancaireService cptService = new CompteBancaireService();
+		if (compte1 != null && client1 != null && compte2 != null && client2 != null && sommeADebiter != null) {
+			resultat = compte1.substring(compte1.lastIndexOf("=") + 1, compte1.length());
+			idCompte1 = Long.parseLong(resultat);
+			resultat = compte2.substring(compte2.lastIndexOf("=") + 1, compte2.length());
+			idCompte2 = Long.parseLong(resultat);
+			montant = Double.parseDouble(sommeADebiter);
+			try {
+				compteDebite = cptService.lireUnCompteBancaire(idCompte1);
+				compteCredite = cptService.lireUnCompteBancaire(idCompte2);
+			} catch (DAOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				cptService.virement(compteDebite, compteCredite, montant);
+			} catch (ServiceException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return "#{navigateBean.redirectToMenuConseiller}";
+
+		} else
+			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalide", "comptes et/ou montant non selectionné.");
 		FacesContext.getCurrentInstance().addMessage(null, msg);
+		return "#{navigateBean.redirectToError}";
+
 	}
 
 }
